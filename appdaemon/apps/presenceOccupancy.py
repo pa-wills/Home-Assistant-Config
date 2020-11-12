eec13c9443ba5d3a418828ab5f4427fcbe67a6f5
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2020-11-12 16:55:01 +1100

    alarm

--- appdaemon/apps/presenceOccupancy.py
+++ appdaemon/apps/presenceOccupancy.py
@@ -7,6 +7,10 @@ import mqttapi as mqtt
 class PresenceOccupancyApp(hass.Hass):
 
 	def initialize(self):
+		self.AlarmNotifier_handler = None
+		self.lastNotificationSent = datetime.datetime.now()
+		self.minsBetweenNotifications = 5
+
 		self.listen_state(self.becameOccupied_callback, "sensor.someone_home", new = "Occupied")
 		self.listen_state(self.becameUnoccupied_callback, "sensor.someone_home", new = "Unoccupied")
 
@@ -17,9 +21,21 @@ class PresenceOccupancyApp(hass.Hass):
 		self.call_service("mqtt/publish", topic = "zigbee2mqtt/motion.hallwayDownstairs/set", payload = "{\"motion_sensitivity\": \"high\"}")
 		self.call_service("mqtt/publish", topic = "zigbee2mqtt/motion.toilet/set", payload = "{\"motion_sensitivity\": \"high\"}")
 
+		self.cancel_listen_state(self.AlarmNotifier_handler)
+
+
 	def becameUnoccupied_callback(self, entity, attribute, old, new, kwargs):
 		# Turn down the sensitivities on Motion Sensors in the common areas.
 		self.call_service("mqtt/publish", topic = "zigbee2mqtt/motion.entranceFoyer/set", payload = "{\"motion_sensitivity\": \"low\"}")
 		self.call_service("mqtt/publish", topic = "zigbee2mqtt/motion.eviesBedroom/set", payload = "{\"motion_sensitivity\": \"low\"}")
 		self.call_service("mqtt/publish", topic = "zigbee2mqtt/motion.hallwayDownstairs/set", payload = "{\"motion_sensitivity\": \"low\"}")
 		self.call_service("mqtt/publish", topic = "zigbee2mqtt/motion.toilet/set", payload = "{\"motion_sensitivity\": \"low\"}")
+
+		self.AlarmNotifier_handler = self.listen_state(onMotion_callback, "group.sensors_motion_indoor_all", new = "on")
+
+	def onMotion_callback(self, entity, attribute, old, new, kwargs):
+		timeSinceLastNotification = ((datetime.datetime.now() - self.lastNotificationSent).seconds)
+		if (timeSinceLastNotification >= (60 * self.minsBetweenNotifications)):
+			self.call_service("notify/petes_ios_devices", title = "Security Alert!", message = "Motion detected despite nobody being home.")
+			self.lastNotificationSent = datetime.datetime.now()
+
