361e236fd5cc1caa5c53fb165b69f0ab7d73094d
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2021-08-29 15:59:05 +1000

    Hmmm, should have invoked call_service

--- appdaemon/apps/sonyBraviaToSonosVolSync.py
+++ appdaemon/apps/sonyBraviaToSonosVolSync.py
@@ -31,4 +31,7 @@ class SonyBraviaToSonosVolSyncApp(hass.Hass):
 
 	def tvVolChange_callback(self, entity, attribute, old, new, kwargs):
 		self.tvVol = self.get_state(entity_id = self.tvEntityName, attribute = "volume_level")
-		self.set_state(self.sonosEntityName, state = "playing", attributes = {"volume_level": self.tvVol})
+		self.call_service("media_player.volume_set", entity_id = self.sonosEntityName, volume_level = self.tvVol)
+#		self.set_state(self.sonosEntityName, state = "playing", attributes = {"volume_level": self.tvVol})
+
+
