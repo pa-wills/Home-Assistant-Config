6c87ef27a76a4d3de666e4cc606e80233aa1c014
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2021-12-19 14:53:43 +1100

    another one

--- appdaemon/apps/heartbeatForWatchdog.py
+++ appdaemon/apps/heartbeatForWatchdog.py
@@ -5,11 +5,11 @@ import requests
 class HeartbeatForWatchdogApp(hass.Hass):
 
 	def initialize(self):
-		url = "https://yvg55bdvze.execute-api.ap-southeast-2.amazonaws.com/Prod/heartbeat"
+		self.url = "https://yvg55bdvze.execute-api.ap-southeast-2.amazonaws.com/Prod/heartbeat"
 		self.run_every(self.sendHeartbeatMessage_callback, "now", 60)
 
 	def sendHeartbeatMessage_callback(self, kwargs):
-		r = requests.get(url)
+		r = requests.get(self.url)
 		if (r.status_code == 200):
 			self.log("Heartbeat sent.")
 
