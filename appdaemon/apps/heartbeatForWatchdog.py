75244e17a407c0e666b327f7583ca55e8a875363
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2021-12-19 14:29:04 +1100

    what am i missing here?

--- appdaemon/apps/heartbeatForWatchdog.py
+++ appdaemon/apps/heartbeatForWatchdog.py
@@ -1,4 +1,15 @@
 import appdaemon.plugins.hass.hassapi as hass
 import datetime
+import requests
 
+class HeartbeatForWatchdogApp(hass.Hass):
+
+	def initialize(self):
+		url = "https://yvg55bdvze.execute-api.ap-southeast-2.amazonaws.com/Prod/heartbeat"
+		self.run_every(self.sendHeartbeatMessage_callback, now, 60)
+
+	def sendHeartbeatMessage_callback(self, entity, attribute, old, new, kwargs):
+		r = requests.get(url)
+		if (r.status_code == 200):
+			self.log("Heartbeat sent.")
 
