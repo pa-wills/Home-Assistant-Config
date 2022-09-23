ca5b180e4521c9a8172c279fddea7ecc46bd3230
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2022-09-23 23:51:42 +1000

    minor correction

--- appdaemon/apps/heartbeatForWatchdog.py
+++ appdaemon/apps/heartbeatForWatchdog.py
@@ -5,7 +5,7 @@ import requests
 class HeartbeatForWatchdogApp(hass.Hass):
 
 	def initialize(self):
-		self.url = "https://api.peterwills.com/Prod/heartbeat"
+		self.url = "https://api.peterwills.com/prod/heartbeat"
 		self.run_every(self.sendHeartbeatMessage_callback, "now", 5 * 60)
 
 	def sendHeartbeatMessage_callback(self, kwargs):
