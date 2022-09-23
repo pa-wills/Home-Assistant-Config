f34a2e917144f6006d30387e107079f162d91aff
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2022-09-23 23:18:02 +1000

    Set heartbeat API value statically

--- appdaemon/apps/heartbeatForWatchdog.py
+++ appdaemon/apps/heartbeatForWatchdog.py
@@ -5,7 +5,7 @@ import requests
 class HeartbeatForWatchdogApp(hass.Hass):
 
 	def initialize(self):
-		self.url = "https://okxuqb9sg2.execute-api.ap-southeast-2.amazonaws.com/Prod/heartbeat"
+		self.url = "https://api.peterwills.com/Prod/heartbeat"
 		self.run_every(self.sendHeartbeatMessage_callback, "now", 5 * 60)
 
 	def sendHeartbeatMessage_callback(self, kwargs):
